58a438fe4311979213e5b837ad806f75f67f82e5
Wes Anderson <Wes Anderson>
2017-01-19 19:48:38 -0800

    Initial commit to start the AdminMenu Branch: Weston

---
+++ src/TriCodeTest/Controllers/MenuCreationController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TriCodeTest.Controllers
+{
+    public class MenuCreationController : Controller
+    {
+        // GET: /MenuCreation/
+        public string Index()
+        {
+            return "";
+        }
+
+        // GET: /MenuCreation/menu
+        public IActionResult menu()
+        {
+            return View();
+        }
+
+        //
+        // GET: /MenuCreation/Welcome/
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
