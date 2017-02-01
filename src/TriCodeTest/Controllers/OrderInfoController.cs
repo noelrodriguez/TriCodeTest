a5a9b0bb94f108b726a56f4f4436e688db8a2680
noelrodriguez <noel.rodz17@gmail.com>
2017-01-31 21:32:21 -0800

    Working on Authentication

--- src/TriCodeTest/Controllers/OrderInfoController.cs
+++ src/TriCodeTest/Controllers/OrderInfoController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,7 @@ using Newtonsoft.Json;
 
 namespace TriCodeTest.Controllers
 {
+    [Authorize(Roles = "Admin, Staff")]
     public class OrderInfoController : Controller
     {
         private readonly ApplicationDbContext _context;
