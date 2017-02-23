068c89fd7f2fb8a0b4c3c01175675d375c2a5bfd
GowAlex <rydact95@gmail.com>
2017-02-22 21:34:22 -0800

    Read database
    Reads from database but user ID restriction does not work yet

--- src/TriCodeTest/Controllers/HistoryController.cs
+++ src/TriCodeTest/Controllers/HistoryController.cs
@@ -6,11 +6,45 @@ using Microsoft.AspNetCore.Mvc;
 using TriCodeTest.Data;
 using TriCodeTest.Models.OrderModels;
 using Microsoft.EntityFrameworkCore;
+using TriCodeTest.Models;
+
+
 
 namespace TriCodeTest.Controllers
 {
     public class HistoryController : Controller
     {
+
+        //private readonly UserManager<ApplicationUser> _userManager;
+        //public HistoryController(UserManager<ApplicationUser> userManager)
+        //{
+        //    _userManager = userManager;
+        //}
+
+        //private readonly ApplicationDbContext _context;
+
+        //public HistoryController(ApplicationDbContext context)
+        //{
+        //    _context = context;
+        //}
+
+        //public async Task<IActionResult> Index()
+        //{
+        //    //var allOrders = await _context.OrderInfo.Include(oi => oi.User).Where(s => s.Status != Models.Status.Cart && s.Status != Models.Status.Completed).ToListAsync();
+        //    var userId = User.UserId;
+
+        //    //HistoryViewModel model = new HistoryViewModel()
+        //    //{
+        //    //    Users = _userManager.Users.ToList(),
+        //    //    Orders = _context.OrderInfo.ToList()
+        //    //};
+
+        //    //var userId = _userManager
+        //    var allOrders = await _context.OrderInfo.ToList();//.Where(o => o.User.Id = UserId);
+        //    //var allOrders = await _context.OrderInfo.Include(oi => oi.User).Where(un => un.User.Id == ).ToListAsync();
+        //    return View(allOrders);
+        //}
+
         private readonly ApplicationDbContext _context;
 
         public HistoryController(ApplicationDbContext context)
@@ -18,11 +52,14 @@ namespace TriCodeTest.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            Order oneOrder = new Order();
-            var allOrders = _context.OrderInfo.Include(oi => oi.User).Where(s => s.Status != Models.Status.Cart && s.Status != Models.Status.Completed).ToListAsync();
-            return View();
+            //var allOrders = await _context.OrderInfo.Include(oi => oi.User).Where(s => s.Status != Models.Status.Cart && s.Status != Models.Status.Completed).ToListAsync();
+            //var userId = User.UserId;
+            //ApplicationUser currentUser = db.Users.FirstOrDefault(x => x.Id == currentUserId);
+            //var allOrders = await _context.OrderInfo.Where(o => o.User.Id = UserId)
+            var allOrders = await _context.OrderInfo.ToListAsync();
+            return View(allOrders);
         }
     }
 }
